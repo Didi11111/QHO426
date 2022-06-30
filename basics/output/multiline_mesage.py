d6b48de02fdecf7eefd26fcb6ac72c3a83edf48e
CernopitchiDian <>
2022-06-30 14:50:21 +0000

    Slash double single

--- basics/output/multiline_mesage.py
+++ basics/output/multiline_mesage.py
@@ -1,5 +1,5 @@
 print("System failure Imminent!")
 print("System rebut has been initiated....")
-print("....rebooting sensory system")
+print("....rebooting sensory system\n")
 print("....rebooting output system")
 print("....rebooting hover system")
