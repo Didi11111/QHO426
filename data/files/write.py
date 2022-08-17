509fcc31d167b0ea016e4e039c882300266d2994
CernopitchiDian <>
2022-08-17 15:10:52 +0000

    Added code to generate a csv file..

--- data/files/write.py
+++ data/files/write.py
@@ -1,17 +1,22 @@
+#creating a new file from old file and sort the data
 def search(file_path):
   print("Searching...", end="")
   sections = []
   books = []
+  #open the specified file(file-path) for reading
   with open(file_path) as file:
+    #for each line in file
     for line in file:
+      # get data from old file (from line start with section) and add to new file into section[]
       if line.startswith("Section"):
         section_name = line.split(":")[1]
         sections.append(section_name.strip())
+      # get data from old file (from line start with somthing else) and add to new file into books[]
       else:
         books.append(line.strip())
   print("Done!")
   return (sections, books)
-
+# saving new file
 def save(file_path, data):
   print("Saving...", end="")
   with open(file_path, "w") as file:
